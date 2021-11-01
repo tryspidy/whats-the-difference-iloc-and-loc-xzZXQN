d6c1317e28017490bc2dfac67ff659495f15f6b7
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-11-01 06:24:52 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+- The .iloc indexer selects only by integer location and works 
+similarly to Python lists.
+
+- The .loc indexer selects only by index label, which is 
+similar to how Python dictionaries work.
